94a2e465352f6e29dd018ba8c309fec63e9aef04
Ibrahim Kurhan <ikurhan@outlook.com>
2021-07-03 22:06:23 +0100

    breaking points added

--- src/theme.js
+++ src/theme.js
@@ -2,7 +2,15 @@ import { createMuiTheme } from '@material-ui/core/styles';
 
 // Create a theme instance.
 const theme = createMuiTheme({
-  
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
 });
 
 export default theme;
