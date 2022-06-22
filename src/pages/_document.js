f63cce2c534a0dbaba6429d8a63ba4c7d33fe8dd
Ibrahim Kurhan <ikurhan@outlook.com>
2022-06-22 09:18:57 +0100

    feat: add a/b testing snipptes

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,9 @@
-
-import React from 'react';
-import Document, { Html, Head, Main, NextScript } from 'next/document';
-import { ServerStyleSheet as StyledComponentSheets } from 'styled-components'
-import { ServerStyleSheets as MaterialUiServerStyleSheets } from '@material-ui/styles'
-import theme from '../theme';
+import React from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet as StyledComponentSheets } from "styled-components";
+import { ServerStyleSheets as MaterialUiServerStyleSheets } from "@material-ui/styles";
+import theme from "../theme";
+import { Helmet } from "react-helmet";
 
 export default class MyDocument extends Document {
   render() {
@@ -11,11 +11,25 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta name="theme-color" content={theme.breakpoints.values.xs} />
-          
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
+          <Helmet>
+            <script type="text/javascript">
+              {`window._vwo_code = window._vwo_code || (function(){
+var account_id=631638,
+settings_tolerance=2000,
+library_tolerance=2500,
+use_existing_jquery=false,
+is_spa=1,
+hide_element='body',
+
+/* DO NOT EDIT BELOW THIS LINE */
+f=false,d=document,code={use_existing_jquery:function(){return use_existing_jquery;},library_tolerance:function(){return library_tolerance;},finish:function(){if(!f){f=true;var a=d.getElementById('_vis_opt_path_hides');if(a)a.parentNode.removeChild(a);}},finished:function(){return f;},load:function(a){var b=d.createElement('script');b.src=a;b.type='text/javascript';b.innerText;b.onerror=function(){_vwo_code.finish();};d.getElementsByTagName('head')[0].appendChild(b);},init:function(){
+window.settings_timer=setTimeout(function () {_vwo_code.finish() },settings_tolerance);var a=d.createElement('style'),b=hide_element?hide_element+'{opacity:0 !important;filter:alpha(opacity=0) !important;background:none !important;}':'',h=d.getElementsByTagName('head')[0];a.setAttribute('id','_vis_opt_path_hides');a.setAttribute('type','text/css');if(a.styleSheet)a.styleSheet.cssText=b;else a.appendChild(d.createTextNode(b));h.appendChild(a);this.load('https://dev.visualwebsiteoptimizer.com/j.php?a='+account_id+'&u='+encodeURIComponent(d.URL)+'&f='+(+is_spa)+'&r='+Math.random());return settings_timer; }};window._vwo_settings_timer = code.init(); return code; }());`}
+            </script>
+          </Helmet>
         </Head>
         <body>
           <Main />
@@ -27,34 +41,32 @@ export default class MyDocument extends Document {
 }
 
 MyDocument.getInitialProps = async (ctx) => {
-
-const styledComponentSheet = new StyledComponentSheets()
-const materialUiSheets = new MaterialUiServerStyleSheets()
-const originalRenderPage = ctx.renderPage;
+  const styledComponentSheet = new StyledComponentSheets();
+  const materialUiSheets = new MaterialUiServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
 
   try {
-  ctx.renderPage = () =>
-    originalRenderPage({
-      enhanceApp: App => props =>
-        styledComponentSheet.collectStyles(
-          materialUiSheets.collect(<App {...props} />),
-        ),
-    })
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) =>
+          styledComponentSheet.collectStyles(
+            materialUiSheets.collect(<App {...props} />)
+          ),
+      });
 
-  const initialProps = await Document.getInitialProps(ctx);
+    const initialProps = await Document.getInitialProps(ctx);
 
-  return {
-    ...initialProps,
-    styles: [
-      <React.Fragment key="styles">
-        {initialProps.styles}
-        {materialUiSheets.getStyleElement()}
-        {styledComponentSheet.getStyleElement()}
-      </React.Fragment>,
-    ],
+    return {
+      ...initialProps,
+      styles: [
+        <React.Fragment key="styles">
+          {initialProps.styles}
+          {materialUiSheets.getStyleElement()}
+          {styledComponentSheet.getStyleElement()}
+        </React.Fragment>,
+      ],
+    };
+  } finally {
+    styledComponentSheet.seal();
   }
-} finally {
-  styledComponentSheet.seal()
-}
-
 };
