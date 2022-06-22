b443aae383b1448c35126982160099627aa9040a
Ibrahim Kurhan <ikurhan@outlook.com>
2022-06-22 14:37:29 +0100

    remove react helmet

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,6 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheet as StyledComponentSheets } from "styled-components";
 import { ServerStyleSheets as MaterialUiServerStyleSheets } from "@material-ui/styles";
 import theme from "../theme";
-import { Helmet } from "react-helmet";
 
 export default class MyDocument extends Document {
   render() {
@@ -15,9 +14,9 @@ export default class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
-          <Helmet>
-            <script type="text/javascript">
-              {`window._vwo_code = window._vwo_code || (function(){
+
+          <script type="text/javascript">
+            {`window._vwo_code = window._vwo_code || (function(){
 var account_id=631638,
 settings_tolerance=2000,
 library_tolerance=2500,
@@ -25,11 +24,10 @@ use_existing_jquery=false,
 is_spa=1,
 hide_element='body',
 
-/* DO NOT EDIT BELOW THIS LINE */
+
 f=false,d=document,code={use_existing_jquery:function(){return use_existing_jquery;},library_tolerance:function(){return library_tolerance;},finish:function(){if(!f){f=true;var a=d.getElementById('_vis_opt_path_hides');if(a)a.parentNode.removeChild(a);}},finished:function(){return f;},load:function(a){var b=d.createElement('script');b.src=a;b.type='text/javascript';b.innerText;b.onerror=function(){_vwo_code.finish();};d.getElementsByTagName('head')[0].appendChild(b);},init:function(){
 window.settings_timer=setTimeout(function () {_vwo_code.finish() },settings_tolerance);var a=d.createElement('style'),b=hide_element?hide_element+'{opacity:0 !important;filter:alpha(opacity=0) !important;background:none !important;}':'',h=d.getElementsByTagName('head')[0];a.setAttribute('id','_vis_opt_path_hides');a.setAttribute('type','text/css');if(a.styleSheet)a.styleSheet.cssText=b;else a.appendChild(d.createTextNode(b));h.appendChild(a);this.load('https://dev.visualwebsiteoptimizer.com/j.php?a='+account_id+'&u='+encodeURIComponent(d.URL)+'&f='+(+is_spa)+'&r='+Math.random());return settings_timer; }};window._vwo_settings_timer = code.init(); return code; }());`}
-            </script>
-          </Helmet>
+          </script>
         </Head>
         <body>
           <Main />
