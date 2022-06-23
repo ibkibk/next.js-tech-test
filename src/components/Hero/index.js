a64600660a8b99f46328642ce475ae7cace2f5c6
Ibrahim Kurhan <ikurhan@outlook.com>
2022-06-23 10:10:30 +0100

    add text

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,25 +1,25 @@
-import React from 'react'
-import { Button} from '@material-ui/core';
-import useStyles from '../../styles/hero';
+import React from "react";
+import { Button } from "@material-ui/core";
+import useStyles from "../../styles/hero";
 
 const Hero = () => {
-    const classes = useStyles();
-    return (
+  const classes = useStyles();
+  return (
     <div className={classes.container}>
       <div className={classes.wrapper}>
-         <h1 className={classes.header}>A modern publising platform</h1>
-         <p className={classes.parag}>Grow your audience and build your online brand</p>
-         <div className={classes.buttonContainer}>
-            <Button className={classes.StartButton}>
-              Start for free 
-            </Button>
-            <Button className={classes.LearnButton}>
-              Learn More
-            </Button>
-         </div>
-       </div>
+        <h1 className={classes.header}>A modern publising platform</h1>
+        <p className={classes.parag}>
+          Grow your audience and build your online brand
+        </p>
+        <div className={classes.buttonContainer}>
+          <Button className={classes.StartButton}>
+            Start for free testing
+          </Button>
+          <Button className={classes.LearnButton}>Learn More</Button>
+        </div>
+      </div>
     </div>
-    )
-}
+  );
+};
 
-export default Hero
+export default Hero;
